e6dc8d56298bc7f7ca47343fb22adb4f698e8c36
Alexandrey <alexandre.yucra@outlook.com>
2020-06-03 19:23:25 -0300

    consuming an UFs api from IBGE

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft} from 'react-icons/fi';
 import { Map, TileLayer, Marker} from 'react-leaflet';
+import axios from 'axios';
 import api from '../../services/api';
 
 import './styles.css';
@@ -14,12 +15,25 @@ interface Item {
     image_url: string;
 }
 
+interface IBGEUFResponse {
+    sigla: string;
+}
+
 const CreatePoint = () => {
     const [items, setItems ] = useState<Item[]>([]);
+    const [ufs, setUfs] = useState<string[]>([]);
 
     useEffect(() => {
         api.get('items').then(response => {
             setItems(response.data);
+        });
+    }, []);
+
+    useEffect(() => {
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+            const ufInitials = response.data.map(uf => uf.sigla);
+
+            setUfs(ufInitials);           
         });
     }, []);
 
@@ -90,6 +104,9 @@ const CreatePoint = () => {
                             <label htmlFor="uf">Estado (uf)</label>
                             <select name="uf" id="uf">
                                 <option value="0">Selecione uma UF</option>
+                                {ufs.map(uf => (
+                                    <option key={uf} value={uf}>{uf}</option>
+                                ))}
                             </select>
                         </div>
 
