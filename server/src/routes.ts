fd576f922d79d622567cdea9f3458bee0246ea2d
Alexandrey <alexandre.yucra@outlook.com>
2020-06-06 02:30:41 -0300

    validation form

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import knex from './database/connection';
+import {celebrate, Joi} from 'celebrate';
 
 import multer from 'multer';
 import multerConfig from './config/multer';
@@ -19,7 +20,24 @@ routes.get('/items', itemsController.index);
 
 
 //cria um ponto de coleta
-routes.post('/points', upload.single('image') ,pointsController.create);
+routes.post('/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    }, {
+        abortEarly: false
+    }),
+    pointsController.create
+);
 
 //lista um ponto de coleta por filtro
 routes.get('/points',pointsController.index);
